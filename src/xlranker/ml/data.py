929b870e8a165fdda110874197f84894dcf9f654
John Elizarraras <elizarrj27@gmail.com>
2025-05-14 13:53:10 -0500

    add ppi documentation

--- src/xlranker/ml/data.py
+++ src/xlranker/ml/data.py
@@ -3,5 +3,10 @@ import polars as pl
 
 
 def load_default_ppi() -> pl.DataFrame:
+    """load default pre-generated table of known PPIs from parquet file into polars DataFrame.
+
+    Returns:
+        pl.DataFrame: Two column database with column names of P1 and P2 where P1 and P2 have a known PPI.
+    """
     ppi_path = files("xlranker.data") / "ppi.parquet"
     return pl.read_parquet(ppi_path)
