f2cd13639e50ed9a79b8daf89aaad7d463f71513
John Elizarraras <elizarrj27@gmail.com>
2025-05-15 15:20:02 -0500

    update read_network_file and read_mapping_table_file to meet new requirements

--- src/xlranker/lib.py
+++ src/xlranker/lib.py
@@ -3,6 +3,7 @@ from typing import Self
 import logging
 
 from xlranker.util.mapping import PeptideMapper
+from xlranker.util.readers import read_network_file
 
 from .bio import Protein, Peptide, ProteinPair, PeptideGroup
 
@@ -34,28 +35,15 @@ class XLDataSet:
         split_by: str = "|",
         split_index: int = 6,
     ) -> Self:
-        all_peptides = set()
-        try:
-            with open(network_path) as r:
-                data = r.read().split("\n")
-            for line in data:
-                if "\t" in line:
-                    vals = line.split("\t")
-                    all_peptides.add(vals[0])
-                    all_peptides.add(vals[1])
-        except IndexError:
-            logger.error(
-                "Index out of bound. Make sure network is in the correct format."
-            )
-            raise IndexError()
-        except FileNotFoundError:
-            logger.error(f"File not found: {network_path}")
-            raise FileNotFoundError
-        all_peptides = list(all_peptides)
+        network = read_network_file(network_path)
+        peptide_sequences = set()
+        for group in network:
+            peptide_sequences.add(group.a.sequence)
+            peptide_sequences.add(group.b.sequence)
         mapper = PeptideMapper(
             mapping_table_path=custom_mapping_path,
             split_by=split_by,
             split_index=split_index,
         )
-        mapping_results = mapper.map_sequences(all_peptides)
+        mapping_results = mapper.map_sequences(list(peptide_sequences))
         pairs = []
--- src/xlranker/util/readers.py
+++ src/xlranker/util/readers.py
@@ -36,21 +36,28 @@ def read_network_file(network_path: str) -> list[PeptideGroup]:
     Returns:
         list[PeptideGroup]: list of PeptideGroup representing the network
     """
-    with open(network_path) as r:
-        text = r.read().split("\n")
-    new_rows = set()  # Track unique rows
-    valid_rows = 0  # Keeps track of number of edges in original file
-    for row in text:
-        if "\t" in row:
-            valid_rows += 1
-            vals = row.split("\t")
-            val_a = vals[0]
-            val_b = vals[1]
-            if val_a > val_b:  # Make sure edges are all sorted the same.
-                temp = val_a
-                val_a = val_b
-                val_b = temp
-            new_rows.add(f"{val_a}\t{val_b}")
+    try:
+        with open(network_path) as r:
+            text = r.read().split("\n")
+        new_rows = set()  # Track unique rows
+        valid_rows = 0  # Keeps track of number of edges in original file
+        for row in text:
+            if "\t" in row:
+                valid_rows += 1
+                vals = row.split("\t")
+                val_a = vals[0]
+                val_b = vals[1]
+                if val_a > val_b:  # Make sure edges are all sorted the same.
+                    temp = val_a
+                    val_a = val_b
+                    val_b = temp
+                new_rows.add(f"{val_a}\t{val_b}")
+    except IndexError:
+        logger.error("Index out of bound. Make sure network is in the correct format.")
+        raise IndexError()
+    except FileNotFoundError:
+        logger.error(f"File not found: {network_path}")
+        raise FileNotFoundError
     duplicate_rows = valid_rows - len(new_rows)  # Count number of duplicated rows
     if duplicate_rows > 0:  # Send warning that duplicate edges were removed.
         logger.warning(
@@ -66,7 +73,7 @@ def read_network_file(network_path: str) -> list[PeptideGroup]:
     return network
 
 
-def read_mapping_table_file(file_path: str) -> list[Peptide]:
+def read_mapping_table_file(file_path: str) -> dict[str, list[str]]:
     """read mapping file where the first column is the peptide sequence and the following columns are proteins that map to that sequence
 
     Args:
@@ -75,7 +82,7 @@ def read_mapping_table_file(file_path: str) -> list[Peptide]:
     try:
         with open(file_path, mode="r") as r:
             raw_text = r.read().split("\n")
-        peptides: list[Peptide] = list()
+        mapping_res: dict[str, list[str]] = dict()
         uniq_sequences: set[str] = set()
         for line in raw_text:
             if "\t" in line:
@@ -87,16 +94,11 @@ def read_mapping_table_file(file_path: str) -> list[Peptide]:
                     )
                 else:
                     uniq_sequences.add(seq)
-                    proteins: list[Protein] = []
-                    for val in vals[1:]:
-                        new_prot = Protein(val)
-                        proteins.append(new_prot)
-                    peptide = Peptide(sequence=seq, mapped_proteins=proteins)
-                    peptides.append(peptide)
-        if len(peptides) == 0:
+                    mapping_res[seq] = vals[1:]
+        if len(mapping_res) == 0:
             logging.error(f"No peptide sequences found in mapping file: {file_path}")
             raise ValueError("No peptide sequence identified")
-        return peptides
+        return mapping_res
     except FileNotFoundError:
         logging.error(f"Could not find mapping table file at {file_path}!")
         raise ValueError("Could not read mapping table: File not found.")
