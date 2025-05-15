5452c16f40bf68dcdad15559c629966e40e81a2b
John Elizarraras <elizarrj27@gmail.com>
2025-05-15 14:56:10 -0500

    add support for custom mapping table

--- src/xlranker/util/mapping.py
+++ src/xlranker/util/mapping.py
@@ -2,30 +2,45 @@ from xlranker.data import get_gencode_fasta
 from Bio import SeqIO
 import logging
 
-logger = logging.getLogger()
+from xlranker.util.readers import read_mapping_table_file
+
+logger = logging.getLogger(__name__)
 
 
 class PeptideMapper:
-    fasta_path: str
+    mapping_table_path: str
     split_by: str
     split_index: int
+    is_fasta: bool
 
     def __init__(
-        self, fasta_path: str | None = None, split_by: str = "|", split_index: int = 6
+        self,
+        mapping_table_path: str | None = None,
+        split_by: str = "|",
+        split_index: int = 6,
+        is_fasta: bool = True,
     ):
-        if fasta_path is None:
+        if mapping_table_path is None:
             logger.info("Using default gencode fasta file for peptide mapping")
-            self.fasta_path = get_gencode_fasta()
+            self.mapping_table_path = get_gencode_fasta()
         else:
             logger.info("Using custom fasta file for peptide mapping")
-            self.fasta_path = fasta_path
+            logging.debug(f"FASTA File Path: {mapping_table_path}")
+            self.mapping_table_path = mapping_table_path
         self.split_by = split_by
         self.split_index = split_index
+        self.is_fasta = is_fasta
 
-    def map_sequences(self, sequences: str) -> dict[str, list[str]]:
+    def map_sequences(self, sequences: list[str]) -> dict[str, list[str]]:
+        if self.is_fasta:
+            return self.map_fasta(sequences)
+        else:
+            return read_mapping_table_file(self.mapping_table_path)
+
+    def map_fasta(self, sequences: list[str]) -> dict[str, list[str]]:
         matches = {}
         logger.info(f"Mapping {len(sequences)} peptide sequences")
-        for record in SeqIO.parse(self.fasta_path, "fasta"):
+        for record in SeqIO.parse(self.mapping_table_path, "fasta"):
             for sequence in sequences:
                 if sequence in record.seq:
                     if sequence not in matches:
