b1ec5083cc9126743becca4fa31f6afb504fc447
John Elizarraras <elizarrj27@gmail.com>
2025-05-15 14:56:22 -0500

    better logging

--- src/xlranker/lib.py
+++ src/xlranker/lib.py
@@ -1,7 +1,12 @@
 from dataclasses import dataclass
 from typing import Self
+import logging
+
+from xlranker.util.mapping import PeptideMapper
 
 from .bio import Protein, Peptide, ProteinPair, PeptideGroup
+
+logger = logging.getLogger(__name__)
 
 
 class XLDataSet:
@@ -29,4 +34,28 @@ class XLDataSet:
         split_by: str = "|",
         split_index: int = 6,
     ) -> Self:
-        all_sequences = None
+        all_peptides = set()
+        try:
+            with open(network_path) as r:
+                data = r.read().split("\n")
+            for line in data:
+                if "\t" in line:
+                    vals = line.split("\t")
+                    all_peptides.add(vals[0])
+                    all_peptides.add(vals[1])
+        except IndexError:
+            logger.error(
+                "Index out of bound. Make sure network is in the correct format."
+            )
+            raise IndexError()
+        except FileNotFoundError:
+            logger.error(f"File not found: {network_path}")
+            raise FileNotFoundError
+        all_peptides = list(all_peptides)
+        mapper = PeptideMapper(
+            mapping_table_path=custom_mapping_path,
+            split_by=split_by,
+            split_index=split_index,
+        )
+        mapping_results = mapper.map_sequences(all_peptides)
+        pairs = []
--- src/xlranker/parsimony/selection.py
+++ src/xlranker/parsimony/selection.py
@@ -1,4 +1,7 @@
 from xlranker.bio import PeptideGroup
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class ParsimonySelector:
