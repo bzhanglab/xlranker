eb1b8f79b02bbf29343697ef00e257cd42cc0ff4
John Elizarraras <elizarrj27@gmail.com>
2025-05-16 14:35:25 -0500

    add parsimony group

--- src/xlranker/parsimony/selection.py
+++ src/xlranker/parsimony/selection.py
@@ -1,11 +1,24 @@
-from xlranker.bio import PeptidePair
+from xlranker.bio import PeptidePair, ProteinPair
+from xlranker.lib import XLDataSet
 import logging
 
 logger = logging.getLogger(__name__)
 
 
+class ParsimonyGroup:
+    protein_pairs: list[ProteinPair]
+    peptide_pairs: list[PeptidePair]
+
+
 class ParsimonySelector:
-    def __init__(
-        self, peptide_groups: list[PeptidePair], mapping_table: dict[str, list[str]]
-    ):
-        pass
+    data_set: XLDataSet
+
+    def __init__(self, data_set: XLDataSet):
+        self.data_set = data_set
+
+    def prioritize_pair(self, peptide_pair: PeptidePair) -> None:
+        peptide_pair.protein_pairs
+
+    def prioritize(self) -> None:
+        for pair in self.data_set.network:
+            pass
