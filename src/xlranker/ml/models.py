809b993728c30d91f0b05a1092c2a32df5f7c515
John Elizarraras <elizarrj27@gmail.com>
2025-05-28 11:57:21 -0500

    add get positives and negatives scaffolding

--- src/xlranker/ml/models.py
+++ src/xlranker/ml/models.py
@@ -7,15 +7,32 @@ Model Process:
     - Random protein pairs that are not selected pairs
 """
 
+import logging
+from xlranker.bio.pairs import ProteinPair, PrioritizationStatus
 from xlranker.lib import XLDataSet
+
+logger = logging.getLogger(__name__)
 
 
 class PrioritizationModel:
+    positives: list[ProteinPair]
     dataset: XLDataSet
 
     def __init__(self, dataset: XLDataSet):
-        pass
+        self.dataset = dataset
+        self.positives = []
 
     def get_positives(self):
-        for peptide in self.dataset.peptides.values():
-            pass
+        for protein_pair in self.dataset.protein_pairs.values():
+            if protein_pair.status == PrioritizationStatus.PARSIMONY_SELECTED:
+                self.positives.append(protein_pair)
+
+    def get_negatives(self, n: int) -> list[ProteinPair]:
+        negatives: list[ProteinPair] = []
+        n_prot = len(self.dataset.proteins.values())
+        if n > (n_prot * (n_prot - 1)) // 2 - len(self.positives):
+            logger.warning(
+                f"n value for get_negatives ({n}) is too large. Setting to maximum value: {(n_prot * (n_prot - 1)) // 2 - len(self.positives)}"
+            )
+            n = (n_prot * (n_prot - 1)) // 2 - len(self.positives)
+        return negatives
