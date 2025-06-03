22cece643ca966502b76bcdad6f9cf74290b16cc
John Elizarraras <elizarrj27@gmail.com>
2025-06-03 11:24:33 -0500

    add config function to load from json

--- src/xlranker/config.py
+++ src/xlranker/config.py
@@ -1,5 +1,23 @@
+from dataclasses import dataclass
+import json
+
+
+@dataclass
 class Config:
-    fragile = True  # Break on any warning
+    fragile: bool = False  # Break on any warning
+    detailed: bool = False  # Show more detailed information about dataset and analysis
 
 
 config = Config()
+
+
+def load_from_json(json_file: str) -> None:
+    """set config to settings in JSON file
+
+    Args:
+        json_file (str): path to JSON file
+    """
+    with open(json_file) as r:
+        json_obj = json.load(r)
+    for key in json_obj:
+        setattr(config, key, json_obj[key])
