bc79f0716b5a9fbd36479e4453c7fb987bde2dd2
John Elizarraras <elizarrj27@gmail.com>
2025-06-05 10:27:21 -0500

    improve documentation

--- src/xlranker/bio/__init__.py
+++ src/xlranker/bio/__init__.py
@@ -7,15 +7,23 @@ __all__ = ["Protein", "Peptide"]
 
 
 class PrioritizationStatus(Enum):
-    NOT_ANALYZED = auto()  # No analysis performed yet
+    """Prioritization status for a protein pair"""
+
+    NOT_ANALYZED = auto()
+    "No analysis performed yet"
 
     # Parsimony-based statuses
-    PARSIMONY_NOT_SELECTED = (
-        auto()
-    )  # Another entity was selected, or cannot be selected
-    PARSIMONY_SELECTED = auto()  # Selected as the representative
-    PARSIMONY_AMBIGUOUS = auto()  # No clear candidate
+
+    PARSIMONY_NOT_SELECTED = auto()
+    "Another entity was selected, or cannot be selected"
+    PARSIMONY_SELECTED = auto()
+    "Selected as the representative"
+    PARSIMONY_AMBIGUOUS = auto()
+    "No clear candidate"
 
     # Machine Learning-based statuses
-    ML_NOT_SELECTED = auto()  # Lower ML score
-    ML_SELECTED = auto()  # Highest ML score
+
+    ML_NOT_SELECTED = auto()
+    "Lower ML score"
+    ML_SELECTED = auto()
+    "Highest ML score"
