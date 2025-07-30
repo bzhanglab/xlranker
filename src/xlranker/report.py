e60fc8284b1ec8328dffe644504e4bcebff97184
John Elizarraras <elizarrj27@gmail.com>
2025-07-30 15:27:25 -0500

    Report function documentation

--- src/xlranker/report.py
+++ src/xlranker/report.py
@@ -11,11 +11,31 @@ from xlranker.status import ReportStatus
 def make_report(
     pairs: list[ProteinPair], status: ReportStatus, output_path: Path
 ) -> None:
+    """Write network of protein pairs that have the level of the inputted status or higher.
+
+    Status order (Highest on top)
+    1. Conservative
+    2. Minimal
+    3. Expanded
+    4. All
+
+    Args:
+        pairs (list[ProteinPair]): list of all protein pairs
+        status (ReportStatus): Minimum status to accept.
+        output_path (Path): output path to save pair to
+
+    """
     valid_pairs = [pair for pair in pairs if pair.report_status <= status]
     write_pair_to_network(valid_pairs, str(output_path))
 
 
 def make_all_reports(pairs: list[ProteinPair]) -> None:
+    """Make reports for all status levels into the output_folder specified in the config.
+
+    Args:
+        pairs (list[ProteinPair]): list of all protein pairs.
+
+    """
     output_folder = Path(config.output).joinpath("reports")
     output_folder.mkdir(exist_ok=True)
     make_report(pairs, ReportStatus.CONSERVATIVE, output_folder / "conservative.tsv")
