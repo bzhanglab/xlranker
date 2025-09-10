449974e582921f8f07afbc8b560995b8af8b354e
John Elizarraras <elizarrj27@gmail.com>
2025-09-10 09:18:34 -0500

    add separator selection and improve example location

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -5,10 +5,12 @@
   import { Separator } from "$lib/components/ui/separator/index.js";
   import { Input } from "$lib/components/ui/input/index.js";
   import * as Card from "$lib/components/ui/card/index.js";
+  import * as Select from "$lib/components/ui/select/index.js";
+  import * as HoverCard from "$lib/components/ui/hover-card/index.js";
   import { cn } from "$lib/utils"; // shadcn helper for conditional classes
   import FileUpload from "$lib/components/FileUpload.svelte";
 
-  import logo from "../public/logo.svg";
+  import logo from "/src/assets/logo.svg";
   import Progress from "$lib/components/ui/progress/progress.svelte";
 
   let peptide_file: File[];
@@ -70,23 +72,35 @@
       >
         <div class="px-4">
           <h3>Peptide Network</h3>
-          Please upload a tab-separated file containing peptide pairs, one pair per
-          line. Each line should contain two peptides separated by a tab character.
+          Please upload a <HoverCard.Root>
+            <HoverCard.Trigger>Peptide Network</HoverCard.Trigger>
+            <HoverCard.Content>
+              <div class="grid gap-2">
+                <h4 class="font-semibold">Example</h4>
+                <pre id="example" class="whitespace-pre-wrap">
+EQLAKLMATLR	IIGLDQVAGM
+EQLAKLMATLR	KGMFR
+EQLAKLMATLR	KGMFR
+EQLDNQLDAK	SLNLKHIK
+EQLDNQLDAY	SLNLKHIK
+</pre>
+              </div>
+            </HoverCard.Content>
+          </HoverCard.Root> file containing peptide pairs, one pair per line. Each
+          line should contain two peptides separated by a tab or comma character.
 
           <div class="grid gap-2">
             <h3>Upload Network</h3>
             <FileUpload bind:files={peptide_file} />
-          </div>
-
-          <div class="grid gap-2">
-            <h4>Example</h4>
-            <pre id="example" class="whitespace-pre-wrap">
-EQLAKLMATLR	IIGLDQVAGM
-EQLAKLMATLR	KGMFR
-EQLAKLMATLR	KGMFR
-EQLDNQLDAYMSKTK	SLNLKHIK
-EQLDNQLDAYMSKTK	SLNLKHIK
-</pre>
+            <h4>Peptide Separator</h4>
+            <Select.Root type="single">
+              <Select.Trigger>Select separator</Select.Trigger>
+              <Select.Content>
+                <Select.Item value="tab">Tab</Select.Item>
+                <Select.Item value="comma">Comma</Select.Item>
+                <Select.Item value="space">Space</Select.Item>
+              </Select.Content>
+            </Select.Root>
           </div>
 
           <h3>Sequence Mapping</h3>
