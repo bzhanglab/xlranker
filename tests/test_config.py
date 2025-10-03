24a9ba6e429feb7ab3bf82ed3575b477c2c2d099
John Elizarraras <elizarrj27@gmail.com>
2025-10-03 14:04:59 -0500

    fix pytest rewriting global config

--- tests/test_config.py
+++ tests/test_config.py
@@ -35,8 +35,6 @@ def test_json_loading(tmp_path):
     """Test if json file loading sets correct config options."""
     temp_file = tmp_path / "xlranker_example.json"
     temp_file.write_text(EXAMPLE_JSON)
-    global xlr_config
-    xlr_config = xlranker.config.Config()
     assert not xlr_config.detailed
     assert not xlr_config.advanced.save_model_files
     assert xlr_config.mapping.fasta_type != "GENCODE"
